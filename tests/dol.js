02324a8514feafb7f721feaca567ba5ef64811dd
coolbong <coolbong.works@gmail.com>
2016-09-07 13:14:59 +0900

    add TLV.parseList() API

--- tests/dol.js
+++ tests/dol.js
@@ -2,7 +2,8 @@
  * Created by coolbong on 2016-05-30.
  */
 var assert = require('assert');
-var DOL = require('../lib/TLV').DOL;
+var TLV = require('../lib/TLV');
+var DOL = TLV.DOL;
 
 exports.dol = {
     'example' : function() {
@@ -49,5 +50,23 @@ exports.dol = {
         var cdol1 = '9F02069F03069F1A0295055F2A029A039C019F37049F35019F45029F4C089F34039F21039F7C14';
         var dol = DOL.parse(cdol1);
         assert(dol.count() == 14);
+    },
+    'toTlv()' : function() {
+        var tlv;
+        var dol = DOL.parse('9F5C089F4005');
+        var list = dol.getList();
+
+        tlv = list[0].toTLV('00 00 00 00 00 00 00 00');
+        assert(tlv instanceof TLV);
+        assert(tlv.getTag() == '9F5C');
+        assert(tlv.getLength() == 8);
+        assert(tlv.getValue() == '0000000000000000');
+
+        tlv = list[1].toTLV('1122334455');
+        assert(tlv instanceof TLV);
+        assert(tlv.getTag() == '9F40');
+        assert(tlv.getLength() == 5);
+        assert(tlv.getValue() == '1122334455');
     }
+
 };
